17bd29e6f1ba68eb2f8f5db8e3103f2db87be3e2
unknown <3213132009@qq.com>
2025-07-06 22:05:41 +0800

    react-shotword项目完整

--- react/wordai/shotword/src/components/PictureCard/index.jsx
+++ react/wordai/shotword/src/components/PictureCard/index.jsx
@@ -1,50 +1,55 @@
-import React, { useState } from 'react';
+import {
+  useState
+} from 'react';
+
 import './style.css';
 
-const PictureCard = () => {
-    const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png');
-    const[word,setWord] = useState('');
-    
-    const updateImageData = (e) => {
-        // html5 的文件上传功能
-        // 可选链操作符
-        //const file = e.target.files?e.target.files[0]:null
-        const file = e.target.files?.[0];
-        if (!file) {return;}
-        console.log(file);
-        //图片预览 I/O操作 异步
-        return new Promise((resolve,reject) =>{
-         // html5
-         const reader =new FileReader();
-         reader.readAsDataURL(file);
-         reader.onload = (e) => {
-            // console.log(reader.result);
-            setImgPreview(reader.result);
-         }
-        })
-        // 创建文件预览
+const PictureCard = (props) => {
+  const {
+    word,
+    audio,
+    uploadImg
+  } = props
+  const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png')
+  const uploadImgData = (e) => {
+    const file = (e.target).files?.[0];
+    if (!file) { return; }
+    return new Promise((resolve, reject) => {
         const reader = new FileReader();
-        reader.onload = (event) => {
-            setImgPreview(event.target.result);
-        };
         reader.readAsDataURL(file);
-    }
-    
-    return (
-        <div className='card'>
-            <input 
-                id="selectImage"
-                type='file'
-                accept='.jpg,.jpeg,.png,.gif'
-                onChange={updateImageData} 
-            />
-            <label className='upload' htmlFor='selectImage'>
-                <img src={imgPreview} alt='preview' />
-            </label>
-            {/* template -> JSX -> {数据绑定} -> 响应式 -> 单词业务 */}
-            <div className='word'>{word}</div>
+        reader.onload = () => {
+            const data = reader.result;
+            setImgPreview(data);
+            uploadImg(data);
+            resolve(data);
+        }
+        reader.onerror = (error) => { reject(error); };
+    })
+  }
+
+  const playAudio = () => {
+    const audioEle = new Audio(audio);
+    audioEle.play();
+  }
+  return (
+    <div className="card">
+      <input 
+        id="selectImage" 
+        type="file" 
+        accept=".jpg,.jpeg,.png,.gif" 
+        onChange={uploadImgData}
+      />
+      <label htmlFor="selectImage" className="upload">
+        <img src={imgPreview} alt="preview" />
+      </label>
+      <div className="word">{word}</div>
+      {audio && (
+        <div className="playAudio" onClick={playAudio}>
+            <img width="20px" src="https://res.bearbobo.com/resource/upload/Omq2HFs8/playA-3iob5qyckpa.png" alt="logo" />
         </div>
-    );
+      )}
+    </div>
+  )
 }
 
 export default PictureCard;
